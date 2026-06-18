4e17c61972e60e04d0717a68995f7704e5a6bdba
Hemanthi Guntuku <hemanthiguntuku@gmail.com>
2026-06-18 18:47:03 +0530

    Remove duplicate zod import

--- backend/src/modules/analytics/routes.js
+++ backend/src/modules/analytics/routes.js
@@ -2,7 +2,6 @@ const { z } = require('zod');
 const auth = require('../../middleware/auth');
 const rbac = require('../../middleware/rbac');
 const repo = require('./repository');
-const { z } = require('zod');
 async function routes(fastify) {
   fastify.get(
     '/overview',
