f6cd80567d557687766b7efc79c29a8900f14fb8
Rana Dev U <ranadevd821@gmail.com>
2026-07-04 12:15:55 +0530

    Fix JSX formatting in Tasks component

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -358,7 +358,6 @@ export default function Tasks() {
                       ))
                     )}
                   </div>
-                )}
               </Card>
             );
           })}
