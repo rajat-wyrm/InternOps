1f8b7f3ae9e03357f314eaaac725a34443bed1f1
hemanthiguntuku <289945675+hemanthiguntuku@users.noreply.github.com>
2026-07-14 14:46:01 +0000

    style: automatically format code with Prettier

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -75,9 +75,9 @@ export default function Profile() {
   const validateProfile = () => {
     const name = fullName.trim();
     if (!/^[A-Za-z ]+$/.test(name)) {
-  setNameError("Name can only contain letters and spaces.");
-  return false;
-}
+      setNameError('Name can only contain letters and spaces.');
+      return false;
+    }
     if (name.length < 3) {
       setNameError('Name must be at least 3 characters.');
       return false;
